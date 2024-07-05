7f6d1f21e507fe6022174941f795a7f52d048b63
Wesperion <121458091+Wesperion@users.noreply.github.com>
2024-07-04 17:25:25 -0700

    Create PatientClass.java
    Patient class to hold patient information:

---
+++ src/application/PatientClass.java
@@ -0,0 +1,18 @@
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String contactInfo;
+    private String insuranceInfo;
+    private String pharmacyInfo;
+    // Add getters and setters as needed
+
+    public Patient(String firstName, String lastName, LocalDate birthday, String contactInfo, String insuranceInfo, String pharmacyInfo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.contactInfo = contactInfo;
+        this.insuranceInfo = insuranceInfo;
+        this.pharmacyInfo = pharmacyInfo;
+    }
+}
