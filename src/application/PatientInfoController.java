be9f4de78136ad5f4332471454a420d65a85a53c
Ariya3 <ariya.shah03@gmail.com>
2024-07-06 14:21:39 -0700

    added id and onChange to flip between screens

--- src/application/PatientInfoController.java
+++ src/application/PatientInfoController.java
@@ -15,37 +15,45 @@ import javafx.stage.Stage;
 public class PatientInfoController {
 	
 	//When this method is called, it will changed to the scene to menu page
-	@FXML
-	public void switchToMenu(ActionEvent event) throws IOException 
-	{
-		Parent PatientMenuParent = FXMLLoader.load(getClass().getResource("PatientMenu.fxml"));
-		Scene PatientMenuScene = new Scene(PatientMenuParent);
-				
-		//This line gets the stage information
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		window.setScene(PatientMenuScene);
-		window.show();
-	}
+	//@FXML
+	/*
+	 * public void switchToMenu(ActionEvent event) throws IOException { Parent
+	 * PatientMenuParent =
+	 * FXMLLoader.load(getClass().getResource("PatientMenu.fxml")); Scene
+	 * PatientMenuScene = new Scene(PatientMenuParent);
+	 * 
+	 * //This line gets the stage information Stage window = (Stage) ((Node)
+	 * event.getSource()).getScene().getWindow(); window.setScene(PatientMenuScene);
+	 * window.show(); }
+	 */
 
-    
+	/*
+	 * @FXML public void switchLogOut(ActionEvent event) throws IOException { Parent
+	 * logOutParent = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+	 * Scene logOutScene = new Scene(logOutParent);
+	 * 
+	 * //This line gets the stage information Stage window = (Stage) ((Node)
+	 * event.getSource()).getScene().getWindow(); window.setScene(logOutScene);
+	 * window.show(); }
+	 */
 
     @FXML
     private TextArea insuranceArea;
 
     @FXML
-    private Button insuranceUpdateButton;
+    private Button updateInsuranceButton;
 
     @FXML
     private TextArea patientInfoArea;
     
     @FXML
-    private Button patientInfoUpdateButton;
+    private Button updateContactButton;
     
     @FXML
     private TextArea pharmacyArea;
 
     @FXML
-    private Button pharmacyUpdateButton;
+    private Button updatePharmacyButton;
     
     @FXML
     private Button logOutButton;
@@ -56,28 +64,40 @@ public class PatientInfoController {
     
     
     @FXML
-    void patientInfoUpdate(ActionEvent event) {
+    void updateContact(ActionEvent event) {
 
     }
     
     @FXML
-    void pharmacyUpdate(ActionEvent event) {
+    void updatePharmacy(ActionEvent event) {
 
     }
 
     @FXML
-    void insuranceUpdate(ActionEvent event) {
+    void updateInsurance(ActionEvent event) {
 
     }
     
     @FXML
-    void backToMenu(ActionEvent event) {
-
+    void backToMenu(ActionEvent event) throws IOException {
+    	Parent PatientMenuParent = FXMLLoader.load(getClass().getResource("PatientMenu.fxml"));
+		Scene PatientMenuScene = new Scene(PatientMenuParent);
+				
+		//This line gets the stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(PatientMenuScene);
+		window.show();
     }
 
     @FXML
-    void logOut(ActionEvent event) {
-
+    void logOut(ActionEvent event) throws IOException {
+		Parent logOutParent = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+		Scene logOutScene = new Scene(logOutParent);
+				
+		//This line gets the stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(logOutScene);
+		window.show();
     }
 
 
